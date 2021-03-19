ad0ef46bbc8a8a60cb808de1f9060063e1216287
Jade <jadeve.jimenez@yahoo.com>
2021-03-19 15:07:13 -0700

    query is done

--- etl_query.sql
+++ etl_query.sql
@@ -1,8 +1,8 @@
 SELECT * FROM games;
 SELECT * FROM meta_game;
 
---Find best PSvita games
-SELECT games.game,games.price,games.owners,games.developer,meta_game.console
+--Find PSvita games with good scores
+SELECT games.game,games.price,games.owners,games.developer,meta_game.console,meta_game.metascore,meta_game.userscore
 FROM games
 JOIN meta_game
 ON games.game = meta_game.game
